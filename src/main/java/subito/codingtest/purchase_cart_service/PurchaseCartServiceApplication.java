6578626329a8408838aea685d1838245a2b87960
Paolo Abatemarco <paolo.abatemarco@gmail.com>
2024-12-14 15:23:35 +0100

    add base controller and scripts for building, testing and running the application

---
+++ src/main/java/subito/codingtest/purchase_cart_service/PurchaseCartServiceApplication.java
@@ -0,0 +1,13 @@
+package subito.codingtest.purchase_cart_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseCartServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchaseCartServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/subito/codingtest/purchase_cart_service/PurchaseCartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package subito.codingtest.purchase_cart_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseCartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
